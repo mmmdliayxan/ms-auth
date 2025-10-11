c597a9bee2bcd74fdf059278ad2c8f957a3bf3ac
Aykhan Mammadli <mmmdliayxan2107@gmail.com>
2025-10-11 22:36:21 +0400

    ci yml is changed again

--- src/main/java/com/example/msauth/config/SecurityConfig.java
+++ src/main/java/com/example/msauth/config/SecurityConfig.java
@@ -2,15 +2,12 @@ package com.example.msauth.config;
 
 
 import com.example.msauth.filter.RoleHeaderAuthenticationFilter;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
