5ae614a4eda1fd428eaf53d3e29ae7fd7ad54246
Aykhan Mammadli <mmmdliayxan2107@gmail.com>
2026-06-05 03:17:12 +0400

    removed unused imports in auth controller

--- src/main/java/com/example/msauth/controller/AuthController.java
+++ src/main/java/com/example/msauth/controller/AuthController.java
@@ -8,8 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.io.IOException;
-import java.lang.RuntimeException;
 
 @RestController
 @RequestMapping("/auth")
